7a6ead503c6d69f499fcb0f8e1203914df3e04be
ROQUIGNY Gabriel <gabriel@roquigny.eu>
2016-06-01 22:07:17 +0200

    Implementation for CardinalDirection

--- src/main/java/kata/rover/CardinalDirection.java
+++ src/main/java/kata/rover/CardinalDirection.java
@@ -1,22 +1,30 @@
 package kata.rover;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.function.Consumer;
 
 public enum CardinalDirection implements Direction {
-    NORTH,
-    SOUTH,
-    EAST,
-    WEST;
+    NORTH("WEST", "EAST"),
+    SOUTH("EAST", "WEST"),
+    EAST("NORTH", "SOUTH"),
+    WEST("SOUTH", "NORTH");
+
+    private String left;
+    private String right;
+
+    CardinalDirection(String left, String right) {
+        this.left = left;
+        this.right = right;
+    }
 
     @Override
     public CardinalDirection giveMeDirectionOnLeftRotation(Consumer<Direction> directionConsumer) {
-        throw new NotImplementedException();
+        directionConsumer.accept(valueOf(left));
+        return this;
     }
 
     @Override
     public CardinalDirection giveMeDirectionOnRightRotation(Consumer<Direction> directionConsumer) {
-        throw new NotImplementedException();
+        directionConsumer.accept(valueOf(right));
+        return this;
     }
 }
