67ebf82455876c7ca982553d250a5e5636d54bf3
ROQUIGNY Gabriel <gabriel@roquigny.eu>
2016-06-02 08:06:32 +0200

    Structure for coordinate
    - Coordinate represent X and Y on the map.

---
+++ src/main/java/kata/rover/Coordinate.java
@@ -0,0 +1,19 @@
+package kata.rover;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.function.Consumer;
+
+public class Coordinate {
+    public Coordinate(Integer x, Integer y) {
+        super();
+    }
+
+    public Coordinate giveMeX(Consumer<Integer> x) {
+        throw new NotImplementedException();
+    }
+
+    public Coordinate giveMeY(Consumer<Integer> y) {
+        throw new NotImplementedException();
+    }
+}
